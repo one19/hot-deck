f9d2965d5cf75b12b89aafc31c1d93bc279d89de
Drew Showalter <10665739+one19@users.noreply.github.com>
2023-12-17 20:44:10 +1100

    :sparkles: add placeholder graphics.

---
+++ src/assets/resources/raw/index.ts
@@ -0,0 +1,15 @@
+import oilURL from './oil.png';
+import coalURL from './coal.png';
+import goldURL from './gold.png';
+import sandURL from './sand.png';
+import rocksURL from './rocks.png';
+import copperURL from './copper.png';
+import magnetiteURL from './magnetite.png';
+
+export const oil = oilURL;
+export const coal = coalURL;
+export const gold = goldURL;
+export const sand = sandURL;
+export const rocks = rocksURL;
+export const copper = copperURL;
+export const magnetite = magnetiteURL;
