614c3211ba8c10f4febd34258168ef6bc9700673
Drew Showalter <10665739+one19@users.noreply.github.com>
2024-02-21 00:34:35 +1100

    🔥 remove testing pile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 import styled from '@emotion/styled';
+import { Global, css } from '@emotion/react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 import ResourceCounter from './zoo/ResourceCounter';
 import { GameIdContext } from './providers/GameId';
 import getMiningCard from './cards/getMiningCard';
-import PlayArea from './PlayArea';
-import Pile from './cards/Pile';
+import MapArea from './MapArea';
 import Hand from './Hand';
 
-const PageContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const globalStyles = css`
+  html,
+  body,
+  #root {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden; /* To prevent scrolling on the page body */
+  }
+
+  body {
+    font-family: sans-serif;
+  }
+`;
+
+const PlayArea = styled.div`
+  display: grid;
   height: 100vh;
   width: 100vw;
+  margin: 0;
   overflow: hidden; /* To prevent scrolling on the page body */
+
+  grid-template-columns: [left] 0 [map-left] 5vw [map-right] 90vw [right] 100vw;
+  grid-template-rows: [top] 0 [map-top] 10% [map-bottom] 90% [bottom];
 
   --card-width: 200px;
   --card-height: 280px;
@@ -31,13 +49,13 @@ const cards = [getMiningCard(), getMiningCard(), getMiningCard(), getMiningCard(
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    <Global styles={globalStyles} />
     <GameIdContext.Provider value="test">
-      <PageContainer>
+      <PlayArea id="root">
         <ResourceCounter />
-        <PlayArea />
-        <Pile infinite facedown cardFactory={getMiningCard} />
+        <MapArea />
         <Hand cards={cards} />
-      </PageContainer>
+      </PlayArea>
     </GameIdContext.Provider>
   </QueryClientProvider>
 );
