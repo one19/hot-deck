2fd3020758f9530bf92bfffc18df0e49c2a13937
Drew Showalter <10665739+one19@users.noreply.github.com>
2024-07-27 18:28:13 +1000

    💩 fix sloppy naming copypasta.

--- src/hooks/resources.tsx
+++ src/hooks/resources.tsx
@@ -1,26 +1,25 @@
-import { useContext } from 'react';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-import { GameIdContext } from '../providers/GameId';
 import { updateResources, getAllResources } from '../controllers/resources';
 import { ResourceState } from '../controllers/types';
+import { useParams } from 'react-router-dom';
 
 export const useSetResources = (resourceVariant: string) => {
   const queryClient = useQueryClient();
-  const gameId = useContext(GameIdContext);
+  const { gameId = 'test' } = useParams<{ gameId: string }>();
 
   return useMutation({
-    mutationKey: ['resources', resourceVariant],
+    mutationKey: ['resources', { id: gameId }, resourceVariant],
     mutationFn: (value: Partial<ResourceState>) => updateResources(value, resourceVariant, gameId),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['resources'] }),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['resources', { id: gameId }] }),
   });
 };
 
 export const useGetAllResources = () => {
-  const gameId = useContext(GameIdContext);
+  const { gameId = 'test' } = useParams<{ gameId: string }>();
 
   return useQuery({
-    queryKey: ['resources'],
+    queryKey: ['resources', { id: gameId }],
     queryFn: () => getAllResources(gameId),
   });
 };
--- src/hooks/settings.tsx
+++ src/hooks/settings.tsx
@@ -1,20 +1,19 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-import { updateSettings, getSettings } from '../controllers/settings';
-import { ResourceState } from '../controllers/types';
+import { updateSettings, getSettings, Settings } from '../controllers/settings';
 
-export const useSettings = (resourceVariant: string) => {
+export const useSettings = () => {
   const queryClient = useQueryClient();
 
   const query = useQuery({
     queryKey: ['settings'],
-    queryFn: () => getSettings(),
+    queryFn: getSettings,
   });
-  const mutation = useMutation({
-    mutationKey: ['resources', resourceVariant],
-    mutationFn: (value: Partial<ResourceState>) => updateSettings(value),
+  const { mutate } = useMutation({
+    mutationKey: ['settings'],
+    mutationFn: (value: Partial<Settings>) => updateSettings(value),
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['settings'] }),
   });
 
-  return [query, mutation];
+  return [query, mutate] as const;
 };
