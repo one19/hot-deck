68e321abfdea867ace819577a472d17d1b8d9e6f
Drew Showalter <10665739+one19@users.noreply.github.com>
2024-07-27 18:29:01 +1000

    💩 add some more card testing components.

---
+++ src/cards/Shaders/unused-test-components/Generic.tsx
@@ -0,0 +1,50 @@
+import { useRef } from 'react';
+import { extend, ReactThreeFiber, useThree } from '@react-three/fiber';
+import { shaderMaterial } from '@react-three/drei';
+import { ShaderMaterial } from 'three';
+
+// Step 1: Define the shader
+const TestMaterial = shaderMaterial(
+  {},
+  // Vertex Shader
+  `
+  varying vec2 vUv;
+
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }`,
+  // Fragment Shader
+  `
+  varying vec2 vUv;
+  
+  void main() {
+    // vec2 newPos = fract(vUv * 10.0); // subdivide our gradient
+  
+    gl_FragColor = vec4(vUv, 1.0, 1.0);
+  }
+  `
+);
+
+extend({ TestMaterial });
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    testMaterial: ReactThreeFiber.Object3DNode<ShaderMaterial, typeof ShaderMaterial>;
+  }
+}
+
+// Step 2: Create the component and pass animated props
+const GenericTestOverlay = () => {
+  const shaderRef = useRef<ShaderMaterial>(null);
+  const { viewport } = useThree();
+
+  return (
+    <mesh scale={[viewport.width, viewport.height, 1]}>
+      <planeGeometry attach="geometry" args={[1, 1]} />
+      <testMaterial ref={shaderRef} attach="material" />
+    </mesh>
+  );
+};
+
+export default GenericTestOverlay;
---
+++ src/cards/Shaders/unused-test-components/PopTransition.tsx
@@ -0,0 +1,66 @@
+import { ReactNode, useEffect, useState } from 'react';
+import { Canvas, extend } from '@react-three/fiber';
+import { SpringValue, animated, useSpring } from '@react-spring/three';
+import { CircleGeometry } from 'three';
+import { Html } from '@react-three/drei';
+
+extend({ CircleGeometry });
+
+type SquircleProps = {
+  position: [number, number, number];
+  scale: number;
+  opacity: SpringValue<number>;
+};
+const Squircle = ({ position, scale, opacity }: SquircleProps) => {
+  return (
+    <animated.mesh position={position} scale={scale}>
+      <circleGeometry attach="geometry" args={[0.2, 32]} />
+      <animated.meshStandardMaterial
+        attach="material"
+        color="hotpink"
+        transparent
+        opacity={opacity}
+      />
+    </animated.mesh>
+  );
+};
+
+const PopEffectWrapper = ({ children }: { children: ReactNode }) => {
+  const [isUnmounting, setIsUnmounting] = useState(false);
+  const { opacity } = useSpring({ opacity: isUnmounting ? 0 : 1, config: { duration: 200 } });
+
+  useEffect(() => {
+    return () => {
+      console.log('unmounting');
+      setIsUnmounting(true);
+    };
+  }, []);
+
+  const squircles = Array.from({ length: Math.floor(Math.random() * 6) + 5 }, () => ({
+    position: [Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5],
+    scale: Math.random() * 0.5,
+  }));
+
+  console.log(isUnmounting);
+
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Html>
+        <div style={{ display: isUnmounting ? 'none' : 'block' }}>{children}</div>
+      </Html>
+      {isUnmounting &&
+        squircles.map((squircle, index) => (
+          <Squircle
+            key={index}
+            position={squircle.position as [number, number, number]}
+            scale={squircle.scale}
+            opacity={opacity}
+          />
+        ))}
+    </Canvas>
+  );
+};
+
+export default PopEffectWrapper;
